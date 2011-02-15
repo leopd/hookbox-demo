7bd95a4d30b05578f3add00a74fea97d48e3b027
Leo Dirac <leo.dirac@gmail.com>
2011-02-15 15:36:27 -0800

    Simplifying code.

--- ows/rubber_stamp.py
+++ ows/rubber_stamp.py
@@ -1,24 +1,14 @@
 import cherrypy
 import json  # requires relatively new python, otherwise simple_json
 
-
-
 class JsonResponse(object):
-    """Basic response to many Hookbox callbacks.
-    """
+    """Sends JSON data to hookbox callbacks."""
     def __init__(self,data):
         self.data = data
 
     def index(self):
         return json.dumps( self.data )
     index.exposed = True
-
-
-class RespondTrue(JsonResponse):
-    def __init__(self):
-        true_response = [ True, {} ]
-        super(RespondTrue,self).__init__( true_response )
-
 
 
 class RubberStamp(object):
@@ -30,16 +20,19 @@ class RubberStamp(object):
         return "Rubber stamp for hookbox auth"
     index.exposed = True
 
-    connect = RespondTrue()
-    disconnect = RespondTrue()
-    subscribe = RespondTrue()
-    unsubscribe = RespondTrue()
-    publish = RespondTrue()
+    true_response = [ True, {} ]
 
-    channel_config = [ True, { "history_size" : 0, 
+    connect = JsonResponse( true_response )
+    disconnect = JsonResponse( true_response )
+    subscribe = JsonResponse( true_response )
+    unsubscribe = JsonResponse( true_response )
+    publish = JsonResponse( true_response )
+
+    # See: http://hookbox.org/docs/javascript.html#channel-attributes
+    channel_attrib = [ True, { "history_size" : 0, 
                                "reflective" : True, 
                                "presenceful" : True } ]
-    create_channel = JsonResponse( channel_config )
+    create_channel = JsonResponse( channel_attrib )
 
 
 cherrypy.quickstart(RubberStamp())
